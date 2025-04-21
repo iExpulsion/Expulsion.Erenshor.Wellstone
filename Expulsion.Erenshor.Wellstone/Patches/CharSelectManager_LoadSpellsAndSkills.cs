2f98b45a97e7a0794a6ef0c6b8bbea7081fd2865
Andrew Harris <31739342+iExpulsion@users.noreply.github.com>
2025-04-21 14:20:15 -0400

    Updated Spell Description

--- Expulsion.Erenshor.Wellstone/Patches/CharSelectManager_LoadSpellsAndSkills.cs
+++ Expulsion.Erenshor.Wellstone/Patches/CharSelectManager_LoadSpellsAndSkills.cs
@@ -8,7 +8,8 @@ namespace Expulsion.Erenshor.Wellstone.Patches
         public static void Postfix(CharSelectManager __instance)
         {
             var spellWellstone = GameData.SpellDatabase.GetSpellByID("Expulsion.Wellstone");
-            
+            spellWellstone.SpellDesc = $"Binded to {GameData.BindZone}\nTeleports to your last Wishing Well location.";
+
             if (!GameData.CurrentCharacterSlot.CharacterSpells.Contains(spellWellstone.Id))
             {
                 Plugin.Log?.LogInfo("Adding Wellstone to SpellBook");
---
+++ Expulsion.Erenshor.Wellstone/Patches/SetBind_SetBindPoint.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace Expulsion.Erenshor.Wellstone.Patches
+{
+    [HarmonyPatch(typeof(SetBind), "SetBindPoint")]
+    public class SetBind_SetBindPoint
+    {
+        public static void Postfix(SetBind __instance)
+        {
+            var spellWellstone = GameData.SpellDatabase.GetSpellByID("Expulsion.Wellstone");
+            spellWellstone.SpellDesc = $"Binded to {GameData.BindZone}\nTeleports to your last Wishing Well location.";
+        }
+    }
+}
